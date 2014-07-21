d20496ea461d138d26e90a9694a1d1a2be62d425
root <root@lamp.(none)>
2014-07-21 23:22:16 +0000

    Inserted about page.

--- frontend/htdocs/includes/sidebar_topics.php
+++ frontend/htdocs/includes/sidebar_topics.php
@@ -4,6 +4,7 @@
 		<ul class="arrows_list1">
 			<li><a href="what-are-bitcoins.php" <?= ($CFG->self == 'what-are-bitcoins.php') ? 'class="active"' : '' ?>><i class="fa fa-angle-right"></i> <?= Lang::string('what-are-bitcoins') ?></a></li>
 			<li><a href="how-bitcoin-works.php" <?= ($CFG->self == 'how-bitcoin-works.php') ? 'class="active"' : '' ?>><i class="fa fa-angle-right"></i> <?= Lang::string('how-bitcoin-works') ?></a></li>
+			<li><a href="about.php" <?= ($CFG->self == 'about.php') ? 'class="active"' : '' ?>><i class="fa fa-angle-right"></i> <?= Lang::string('about') ?></a></li>
 			<li><a href="how-to-register.php" <?= ($CFG->self == 'how-to-register.php') ? 'class="active"' : '' ?>><i class="fa fa-angle-right"></i> <?= Lang::string('how-to-register') ?></a></li>
 			<li><a href="securing-account.php" <?= ($CFG->self == 'securing-account.php') ? 'class="active"' : '' ?>><i class="fa fa-angle-right"></i> <?= Lang::string('securing-account') ?></a></li>
 			<li><a href="reset_2fa.php" <?= ($CFG->self == 'reset_2fa.php') ? 'class="active"' : '' ?>><i class="fa fa-angle-right"></i> <?= Lang::string('reset-2fa') ?></a></li>
