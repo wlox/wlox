86e426d6ffa82c992c90c3b24e3e3c5312b9cc5a
root <root@lamp.(none)>
2014-10-27 16:46:12 +0000

    Seperated script for getting historical Bitcoin prices into new file.

--- cron/get_stats.php
+++ cron/get_stats.php
@@ -60,42 +60,4 @@ if ($CFG->currencies) {
 
 echo 'done'.PHP_EOL;
 
-/*
-// QUANDL HISTORICAL DATA
-$currency = 'USD';
-$exchange = 'BITSTAMP';
-
-$data = file_get_contents('http://www.quandl.com/api/v1/datasets/BITCOIN/'.$exchange.$currency.'.csv?trim_start=2011-01-01');
-$data1 = explode("\n",$data);
-if ($data1) {
-	$i = 1;
-	$c = count($data1);
-	foreach ($data1 as $row) {
-		if ($i == 1) {
-			$i++;
-			continue;
-		}
-		
-		$row1 = explode(',',$row);
-		
-		if ($i == 2) {
-			db_update('currencies',$CFG->btc_currency_id,array('usd_ask'=>$row1[4],'usd_bid'=>$row1[4]));
-		}
-		
-		$sql = "SELECT * FROM historical_data WHERE `date` = '{$row1[0]}'";
-		$result = db_query_array($sql);
-		
-		if (!$result) {
-			db_insert('historical_data',array('date'=>$row1[0],strtolower($currency)=>$row1[4]));
-		}
-		else {
-			db_update('historical_data',$result[0]['id'],array(strtolower($currency)=>$row1[4]));
-		}
-		
-		$i++;
-	}
-}
-echo 'done';
-*/
-
 
