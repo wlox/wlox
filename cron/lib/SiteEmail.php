3ee25d6ac4d6f27e30da7f2c7bac0269fe89a737
root <moises@organic.com.pa>
2015-02-11 23:24:27 +0000

    Updated class to be the same as that on API.

--- cron/lib/SiteEmail.php
+++ cron/lib/SiteEmail.php
@@ -10,8 +10,10 @@ class SiteEmail{
 		$sql="SELECT * FROM emails WHERE emails.key='$key' ";	
 		$result = db_query_array($sql);
 		
-		$result[0]['title'] = ($CFG->language == 'en' && !empty($result[0]['title_en'])) ? $result[0]['title_en']: $result[0]['title_es'];
-		$result[0]['content'] = ($CFG->language == 'en' && !empty($result[0]['content_en'])) ? $result[0]['content_en']: $result[0]['content_es'];
+		$CFG->language = (empty($CFG->language)) ? 'en' : $CFG->language;
+		
+		$result[0]['title'] = $result[0]['title_'.$CFG->language];
+		$result[0]['content'] = $result[0]['content_'.$CFG->language];
 		$result[0]['title'] = str_replace('[exchange_name]',$CFG->exchange_name,$result[0]['title']);
 		$result[0]['content'] = str_replace('[exchange_name]',$CFG->exchange_name,$result[0]['content']);
 		return $result[0];
@@ -37,7 +39,7 @@ class SiteEmail{
 		if (User::$info['id'] > 0)
 			$contact_info['user_id'] = User::$info['id'];
 		
-		return Email::send($contact_info['email'],$CFG->contact_email,$email['title'],$CFG->form_email_from,false,$email['content'],$contact_info);
+		return Email::send($contact_info['email'],'contact@1btcxe.com',$email['title'],$CFG->form_email_from,false,$email['content'],$contact_info);
 	}
 }
 ?>
