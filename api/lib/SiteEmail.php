6d9239c2ef439e91ee8d9487f2e5e15b27fa9139
root <moises@organic.com.pa>
2015-02-11 23:21:50 +0000

    Now selecting appropriate languages. Changed regex to allow non-latin.

--- api/lib/SiteEmail.php
+++ api/lib/SiteEmail.php
@@ -10,8 +10,10 @@ class SiteEmail{
 		$sql="SELECT * FROM emails WHERE emails.key='$key' ";	
 		$result = db_query_array($sql);
 		
-		$result[0]['title'] = ($CFG->language == 'en' && !empty($result[0]['title_en'])) ? $result[0]['title_en']: $result[0]['title_es'];
-		$result[0]['content'] = ($CFG->language == 'en' && !empty($result[0]['content_en'])) ? $result[0]['content_en']: $result[0]['content_es'];
+		$CFG->language = (empty($CFG->language)) ? 'en' : $CFG->language;
+		
+		$result[0]['title'] = $result[0]['title_'.$CFG->language];
+		$result[0]['content'] = $result[0]['content_'.$CFG->language];
 		$result[0]['title'] = str_replace('[exchange_name]',$CFG->exchange_name,$result[0]['title']);
 		$result[0]['content'] = str_replace('[exchange_name]',$CFG->exchange_name,$result[0]['content']);
 		return $result[0];
