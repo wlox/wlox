d24533b9320a189856442804a4512a04ce8b0911
root <moises@organic.com.pa>
2015-05-26 20:12:10 +0000

    Accidentally re-enabled the problem email.

--- cron/common.php
+++ cron/common.php
@@ -53,7 +53,7 @@ if ($CFG->self != 'get_stats.php') {
 			$last_notify = time();
 			
 			$content = 'Script '.$CFG->self.' has been waiting for'.(time() - $start_time).'s for '.$lock_file_info[0].' ('.(time() - $lock_file_info[1]).'s) to complete.';
-			Email::send($CFG->support_email,$CFG->support_email,'Cron Bottleneck @'.$CFG->self,$CFG->exchange_name.' Cron',false,$content);
+			//Email::send($CFG->support_email,$CFG->support_email,'Cron Bottleneck @'.$CFG->self,$CFG->exchange_name.' Cron',false,$content);
 			trigger_error($content,E_USER_WARNING);
 		}
 	}
