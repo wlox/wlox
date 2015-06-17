69c19739d4c897e23a4afec56b8b899e54722baa
root <moises@organic.com.pa>
2015-06-17 17:30:51 +0000

    Changed welcome line to something less ominous.

--- frontend/htdocs/account.php
+++ frontend/htdocs/account.php
@@ -55,7 +55,7 @@ include 'includes/head.php';
 	<div class="content_right">
 		<div class="testimonials-4">
 			<? Messages::display(); ?>
-            <h2><?= Lang::string('account-welcome') ?>, <strong><?= (empty(User::$info['first_name'])) ? User::$info['user'] : User::$info['first_name'].' '.User::$info['last_name'] ?></strong></h2>
+            <h2><?= (empty(User::$info['first_name'])) ? Lang::string('account-welcome-anon').' <strong>'.User::$info['user'] : Lang::string('account-welcome').', <strong>'.User::$info['first_name'].' '.User::$info['last_name'] ?></strong></h2>
 			<? 
 			if (User::$info['verified_authy'] != 'Y' && User::$info['verified_google'] != 'Y') {
 				echo '<div class="notice"><div class="message-box-wrap">'.Lang::string('account-security-notify').'</div></div>';
