0f390fe75766f8b955d4e4fcf77c6299534123a0
root <moises@organic.com.pa>
2015-06-22 11:52:23 +0000

    Removed obsolete method.

--- frontend/lib/Content.php
+++ frontend/lib/Content.php
@@ -1,19 +1,5 @@
 <?php
 class Content {
-	public static function getRecord($url){
-		global $CFG;
-		
-		if(empty($url))
-			return false;
-			
-		$sql = "SELECT * FROM content WHERE url = '$url' ";
-		$result = db_query_array($sql);
-		
-		$result[0]['title'] = (empty($result[0]['title_'.$CFG->language])) ? $result[0]['title']: $result[0]['title_'.$CFG->language];
-		$result[0]['content'] = (empty($result[0]['content_'.$CFG->language])) ? $result[0]['content']: $result[0]['content_'.$CFG->language];
-		return $result[0];				
-	}
-	
 	public static function pagination($link_url,$page,$total_rows,$rows_per_page=0,$max_pages=0,$pagination_label=false,$target_elem=false) {
 		global $CFG;
 	
