db4bf0892e5f5f4f557a53d304e952387c9d0424
root <moises@organic.com.pa>
2015-07-08 15:15:29 +0000

    Removed unused function.

--- api/lib/Transactions.php
+++ api/lib/Transactions.php
@@ -143,35 +143,4 @@ class Transactions {
 		$sql = "SELECT * FROM transaction_types ORDER BY id ASC ";
 		return db_query_array($sql);
 	}
-
-	public static function getHistory($currency=false,$type=false) {
-		global $CFG;
-		
-		if (!$CFG->session_active)
-			return false;
-		
-		$type = preg_replace("/[^0-9]/", "",$type);
-		$currency = preg_replace("/[^a-zA-Z]/", "",$currency);
-		
-		$user = User::$info['id'];
-		$sql = "
-		SELECT transactions.*, UNIX_TIMESTAMP(transactions.date) AS datestamp, currencies.currency AS currency, (currencies.usd_ask * transactions.fiat) AS usd_amount, transactions.btc_price AS fiat_price, currencies.fa_symbol AS fa_symbol ".(($user > 0) ? ",IF(transactions.site_user = $user,transaction_types.name_{$CFG->language},transaction_types1.name_{$CFG->language}) AS type, IF(transactions.site_user = $user,transactions.fee,transactions.fee1) AS fee, IF(transactions.site_user = $user,transactions.btc_net,transactions.btc_net1) AS btc_net" : "")."
-		FROM transactions
-		LEFT JOIN transaction_types ON (transaction_types.id = transactions.transaction_type)
-		LEFT JOIN transaction_types transaction_types1 ON (transaction_types1.id = transactions.transaction_type1)
-		LEFT JOIN currencies ON (currencies.id = transactions.currency)
-		WHERE 1";
-		
-		if ($type > 0)
-			$sql .= " AND IF(transactions.site_user = $user,transactions.transaction_type,transactions.transaction_type1) = $type ";
-		if ($currency)
-			$sql .= " AND currencies.currency = '$currency' ";
-		
-		$sql .= "
-		AND transactions.site_user = $user
-		ORDER BY transactions.date DESC LIMIT 0,1 ";
-		
-		$result = db_query_array($sql);
-		return $result[0];
-	}
 }
