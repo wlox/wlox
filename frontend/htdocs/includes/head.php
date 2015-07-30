f790ee6df1aa3e64de82a36895fc28f64fd03187
root <moises@organic.com.pa>
2015-07-30 17:07:39 +0000

    Fix for buttons on home banner.

--- frontend/htdocs/includes/head.php
+++ frontend/htdocs/includes/head.php
@@ -35,7 +35,7 @@
     <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
     
     <!-- responsive devices styles -->
-	<link rel="stylesheet" media="screen" href="css/responsive-leyouts.css?v=20150624" type="text/css" />
+	<link rel="stylesheet" media="screen" href="css/responsive-leyouts.css?v=20150730" type="text/css" />
     
 <!-- just remove the below comments witch color skin you want to use -->
     <!--<link rel="stylesheet" href="css/colors/lightblue.css" />-->
