3fc11c3e21c6ee4ba52a38ef7942fbb6d71a9025
mbassan <moises@organic.com.pa>
2015-08-27 13:27:20 -0500

    Update update_v1.13.2.sql

--- update_v1.13.2.sql
+++ update_v1.13.2.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `status` ADD `btc_24h` DOUBLE( 10, 2 ) NOT NULL DEFAULT '0' AFTER `withdrawals_status`;
+ALTER TABLE `status` ADD `btc_24h` DOUBLE( 10, 2 ) NOT NULL DEFAULT '0';
 
 UPDATE`status` SET `db_version` = '1.13.2' WHERE `status`.`id` =1;
