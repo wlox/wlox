dc56857aa7e6d59774e43bcffef9a40774a656b7
root <moises@organic.com.pa>
2015-08-31 17:29:32 +0000

    Added hex2bin fallback for PHP version < 5.4.

--- api/lib/stdlib.php
+++ api/lib/stdlib.php
@@ -46,4 +46,16 @@ if (!function_exists('mb_strlen')) {
 
 	}
 }
+
+if (!function_exists('hex2bin')) {
+	function hex2bin( $str ) {
+		$sbin = "";
+		$len = strlen( $str );
+		for ( $i = 0; $i < $len; $i += 2 ) {
+			$sbin .= pack( "H*", substr( $str, $i, 2 ) );
+		}
+
+		return $sbin;
+	}
+}
 ?>
